dea214dd271a42c75294d4044f2ec97ad8dea969
KevinCamos <75848615+KevinCamos@users.noreply.github.com>
2021-11-01 19:21:37 +0100

    Update ejecutame.sh

--- ejecutame.sh
+++ ejecutame.sh
@@ -9,12 +9,12 @@ docker run -d -p 4200:4200 --name angular_frontend --network bualabob  frontend
 
 #MONGO
 #Creamos la imágen de mongo
-docker build -t mongo-contain .
-#Creamos el contenedor de frontend
-docker run -d  -p 27017:27017 --name mongo-contain --network bualabob  mongo
+docker build -t mongo-image .
+#Creamos el contenedor de mongo
+docker run -d  -p 27017:27017 --name mongo-contain --network bualabob mongo-image
 
 #BACKEND
-#Creamos la imágen de mongo
+#Creamos la imágen de backend
 docker build -t backend ./servidor
-#Creamos el contenedor de frontend
+#Creamos el contenedor de backend
 docker run -p 4000:4000 --name backend_contenedor --network bualabob backend
